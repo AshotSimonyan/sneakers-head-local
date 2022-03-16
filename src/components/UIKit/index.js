20b701ee967225e3138d20b0aba8f856c0df5585
AshotUltraLabs <a.simonyan.stdev@gmail.com>
2022-03-16 18:07:22 +0400

    coming soon

--- src/components/UIKit/index.js
+++ src/components/UIKit/index.js
@@ -1,5 +1,2 @@
 export { Button } from "./Button/Button"
-export { Input } from "./Input/Input"
-export { Textarea } from "./Input/Textarea"
 export { Icon } from "./Icons/Icon"
-export { Carousel } from "./Carousel/Carousel"
